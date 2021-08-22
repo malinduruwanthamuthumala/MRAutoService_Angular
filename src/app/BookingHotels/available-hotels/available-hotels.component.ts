c3cc6e58f7d5c9a2d96998f17edcb99dde1ec29c
malinduruwantha <malinduruwantha@gmail.com>
2021-08-22 01:54:12 -0700

    added hotel list to frontend

--- src/app/BookingHotels/available-hotels/available-hotels.component.ts
+++ src/app/BookingHotels/available-hotels/available-hotels.component.ts
@@ -13,4 +13,33 @@ export class AvailableHotelsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  hotels: Hotel[] =[
+
+    {
+      _id: '000001',
+    title: 'blue beach',
+    city: "Matara",
+    street: "mahawela road",
+    category: "hotel",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.agoda.com%2Fblue-beach-villa%2Fhotel%2Fpanadura-lk.html&psig=AOvVaw2FVADYNHDRqA-o3L0dzN7I&ust=1629707846593000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiVgP2cxPICFQAAAAAdAAAAABAD",
+    numOfRooms:12,
+    description: "five starts",
+    dailyPrice: 6500,
+    shared: false,
+    createdAt: "2020",
+    },
+    {
+      _id: '000002',
+      title: 'beautiful beach',
+      city: "Matara",
+      street: "nilwella",
+      category: "villa",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hotels.com%2Fho539890%2Fblue-beach-wadduwa-sri-lanka%2F&psig=AOvVaw2FVADYNHDRqA-o3L0dzN7I&ust=1629707846593000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiVgP2cxPICFQAAAAAdAAAAABAO",      numOfRooms:4,
+      description: "five starts",
+      dailyPrice: 3500,
+      shared: false,
+      createdAt: "2020",
+    }
+  ]
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvailableHotelsComponent } from './BookingHotels/available-hotels/available-hotels.component';
 import { ServiceBookingComponent } from './service-booking/service-booking.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 const routes: Routes = [
   { path: 'servicebooking', component: ServiceBookingComponent   },
-  { path: 'userregistration/:userID', component:  UserRegistrationComponent }
+  { path: 'userregistration/:userID', component:  UserRegistrationComponent },
+  { path: 'hotelList', component:  AvailableHotelsComponent }
 ];
 
 @NgModule({ 
