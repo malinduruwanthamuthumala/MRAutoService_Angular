c9b6789ceb921694e1f37f5ad547e14c32f2114d
malinduruwantha <malinduruwantha@gmail.com>
2021-08-24 10:04:40 -0700

    subscribe to hotel service observable

--- src/app/BookingHotels/available-hotels/available-hotels.component.ts
+++ src/app/BookingHotels/available-hotels/available-hotels.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hotel} from '../hotel.model';
+import { HotelService } from '../../shared/hotel.service';
 
 @Component({
   selector: 'app-available-hotels',
@@ -8,38 +9,18 @@ import {Hotel} from '../hotel.model';
 })
 export class AvailableHotelsComponent implements OnInit {
 
-  constructor() { }
+  hotels:Hotel[] = [] ;
+  constructor(private hotelService:HotelService) { 
 
-  ngOnInit(): void {
   }
 
-  hotels: Hotel[] =[
+  ngOnInit(): void {
+    this.hotelService.getHotels()
+      .subscribe((hotels:Hotel[])=>{
+        this.hotels = hotels; 
+    })
+  }
 
-    {
-      _id: '000001',
-    title: 'blue beach',
-    city: "Matara",
-    street: "mahawela road",
-    category: "hotel",
-    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.agoda.com%2Fblue-beach-villa%2Fhotel%2Fpanadura-lk.html&psig=AOvVaw2FVADYNHDRqA-o3L0dzN7I&ust=1629707846593000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiVgP2cxPICFQAAAAAdAAAAABAD",
-    numOfRooms:12,
-    description: "five starts",
-    dailyPrice: 6500,
-    shared: false,
-    createdAt: "2020",
-    },
-    {
-      _id: '000002',
-      title: 'beautiful beach',
-      city: "Matara",
-      street: "nilwella",
-      category: "villa",
-      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hotels.com%2Fho539890%2Fblue-beach-wadduwa-sri-lanka%2F&psig=AOvVaw2FVADYNHDRqA-o3L0dzN7I&ust=1629707846593000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiVgP2cxPICFQAAAAAdAAAAABAO",      numOfRooms:4,
-      description: "five starts",
-      dailyPrice: 3500,
-      shared: false,
-      createdAt: "2020",
-    }
-  ]
+  
 
 }
--- src/app/shared/hotel.service.ts
+++ src/app/shared/hotel.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { observable } from 'rxjs';
+import { Observable, observable } from 'rxjs';
 import { AvailableHotelsComponent } from '../BookingHotels/available-hotels/available-hotels.component';
+import { Hotel } from '../BookingHotels/hotel.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,56 @@ import { AvailableHotelsComponent } from '../BookingHotels/available-hotels/avai
 export class HotelService {
 
   constructor() { }
+
+  hotels: Hotel[] =[
+
+    {
+      _id: '000001',
+    title: 'blue beach',
+    city: "Matara",
+    street: "mahawela road",
+    category: "hotel",
+    image: "https://www.hotelj.lk/wp-content/uploads/2018/04/hotelj-brand-banner-desktop.jpg",
+    numOfRooms:12,
+    description: "five starts",
+    dailyPrice: 6500,
+    shared: false,
+    createdAt: "2020",
+    },
+    {
+      _id: '000002',
+      title: 'beautiful beach',
+      city: "Matara",
+      street: "nilwella",
+      category: "villa",
+      image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/1a/ea/54/hotel-presidente-4s.jpg?w=1200&h=-1&s=1",      numOfRooms:4,
+      description: "five starts",
+      dailyPrice: 3500,
+      shared: false,
+      createdAt: "2020",
+    },
+    {
+      _id: '000003',
+      title: 'ABC beach ',
+      city: "Matara",
+      street: "dickwella",
+      category: "villa",
+      image: "https://www.hotelj.lk/wp-content/uploads/2018/04/hotelj-brand-banner-desktop.jpg", 
+      numOfRooms:4,
+      description: "five starts",
+      dailyPrice: 3500,
+      shared: false,
+      createdAt: "2020",
+    }
+  ]
+
+  public getHotels():Observable<Hotel[]>{
+    return new Observable((observer)=>{
+      setTimeout(()=>{
+        observer.next(this.hotels);
+      },2000)
+    });
+    
+    
+  }
 }
