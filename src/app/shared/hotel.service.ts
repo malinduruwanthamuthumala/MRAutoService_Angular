8e35a59633ef9e0b09ead18f94c9034a7d528131
malinduruwantha <malinduruwantha@gmail.com>
2021-08-24 09:15:39 -0700

    added hotels service

---
+++ src/app/shared/hotel.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
+import { AvailableHotelsComponent } from '../BookingHotels/available-hotels/available-hotels.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelService {
+
+  constructor() { }
+}
